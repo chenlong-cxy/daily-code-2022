a2a4f7fa012567b3890d336e08524fa1094d076e
chenlong-xcy <chenlonghyl@163.com>
2022-01-27 20:44:52 +0800

    小朋友排队

---
+++ test_2022_1_20/test_2022_1_20/test.cpp
@@ -0,0 +1,19 @@
+//–°≈Û”—≈≈∂”
+#include <iostream>
+#include <vector>
+using namespace std;
+
+int main()
+{
+	int n = 0;
+	cin >> n;
+	vector<int> v;
+	for (int i = 0; i < n; i++)
+	{
+		int hight = 0;
+		cin >> hight;
+		v.push_back(hight);
+	}
+	
+	return 0;
+}
